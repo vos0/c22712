98610da9b3bcef64a4f183fa197906cb0624997b
KulishEV <kulishevgeniy@mail.ru>
2023-02-21 13:55:38 +0300

    задание 1

---
+++ task1/Задание.py
@@ -0,0 +1,21 @@
+
+
+from bs4 import BeautifulSoup
+from selenium.webdriver import Chrome
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+# Selenium - библиотека для автоматизации действий веб браузера, скрапинга
+# запускаем браузер
+s = Service('C:\data\hrome\chromedriver.exe')
+browser = webdriver.Chrome(service=s)
+browser.get('https://www.kinopoisk.ru/lists/movies/top250/')
+html_text = browser.page_source
+soup = BeautifulSoup(html_text, 'lxml')
+films=soup.find_all('div', class_='base-movie-main-info_mainInfo__ZL_u3')
+print (films[0].text)
+
+# Это пример парсинга. Вам необходимо спарсить 1 страницу каталога любого сайта на выбор.
+# Спарсить необходимо только Заголовки и описание.
+
+
