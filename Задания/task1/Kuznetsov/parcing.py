5a13ad4255e3f80b427e0bc0c95da58ff88a0ef0
13danny13 <124861906+13danny13@users.noreply.github.com>
2023-03-22 22:45:38 +0300

    Update parcing.py

--- Задания/task1/Kuznetsov/parcing.py
+++ Задания/task1/Kuznetsov/parcing.py
@@ -8,7 +8,7 @@ browser = webdriver.Chrome(service=s)
 browser.get('https://trial-sport.ru/gds.php?s=51516&c1=1070639&c2=1070640')
 html_text = browser.page_source
 soup = BeautifulSoup(html_text, 'lxml')
-bike_name=soup.find_all('a', class_='title')
-print (bike_name[0].text)
+product_name=soup.find_all('a', class_='title')
 description=soup.find_all('span', class_='description')
-print (description[0].text)
+for product_name, description in zip(product_name, description):
+    print(f"Продукт: {product_name.text} \nОписание: {description.text}")
