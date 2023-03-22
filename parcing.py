6182e6dde01b90f3064d0b6eeaaaa28d793ed7bc
13danny13 <kdaniil13052004@gmail.ru>
2023-03-22 22:35:07 +0300

    parcing

--- parcing.py
+++ parcing.py
@@ -8,7 +8,7 @@ browser = webdriver.Chrome(service=s)
 browser.get('https://trial-sport.ru/gds.php?s=51516&c1=1070639&c2=1070640')
 html_text = browser.page_source
 soup = BeautifulSoup(html_text, 'lxml')
-bike_name=soup.find_all('a', class_='title')
+product_name=soup.find_all('a', class_='title')
 description=soup.find_all('span', class_='description')
-for bike_name, description in zip(bike_name, description):
-    print(f"Продукт: {bike_name.text} \nОписание: {description.text}")
+for product_name, description in zip(product_name, description):
+    print(f"Продукт: {product_name.text} \nОписание: {description.text}")
