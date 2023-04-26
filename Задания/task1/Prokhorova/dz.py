b0738b27ec7810a7c108777cc65d1d2af608b232
inweems <prokhorovaviktoria1@gmail.com>
2023-04-26 22:00:09 +0300

    botparsing

--- Задания/task1/Prokhorova/dz.py
+++ Задания/task1/Prokhorova/dz.py
@@ -1,8 +1,9 @@
-import psycopg2
+    import psycopg2
 import wget
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+
 connection = psycopg2.connect(host='localhost', dbname='dbdata', user='postgres', password='Q1w2e3r4')
 
 cursor = connection.cursor()
