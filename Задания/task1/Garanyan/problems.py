e4f0ef9af29efd35925ee52078cb7a65a77b34f3
Harutyun <arutgaranyan2005@gmail.com>
2023-04-26 23:03:28 +0300

    maybe last version

--- Задания/task1/Garanyan/problems.py
+++ Задания/task1/Garanyan/problems.py
@@ -3,21 +3,19 @@ import wget
 
 from Задания.task1.Garanyan import parser
 
-connection = psycopg2.connect(host='localhost', dbname='dbdata', user='postgres',
-                              password='Q1w2e3r4')
+connection = psycopg2.connect(host='localhost', dbname='dbdata', user='postgres', password='Q1w2e3r4')
 cursor = connection.cursor()
-create_q = '''CREATE TABLE Food  
-     (ID serial primary key, Name varchar(500), Price varchar(60), src varchar(100))'''
+create_q = '''CREATE TABLE Food (ID serial primary key, Name varchar(500), Price varchar(60), src varchar(100))'''
 
 try:
     cursor.execute(create_q)
     connection.commit()
 except psycopg2.errors.DuplicateTable:
-    print("s")
+    print("duplicate error")
 
 for j in range(15):
     url = parser.Image[j].find('img').attrs['src']
-    tempf = f"C:\\Users\\Harutyun\\Desktop\\prog\\food{j}.jpg"
+    tempf = f"img\\food{j}.jpg"
     wget.download(url, tempf)
     try:
         insert_query = f'''INSERT into public.Food(Name, Price, src) values ('{parser.Name[j].text}', '{parser.Price[j].text}', '{tempf}') '''
--- Задания/task1/Garanyan/tgbot.py
+++ Задания/task1/Garanyan/tgbot.py
@@ -3,8 +3,7 @@ import psycopg2
 from telebot import types
 import random
 
-connection = psycopg2.connect(host='localhost', dbname='dbdata', user='postgres',
-                              password='Q1w2e3r4')
+connection = psycopg2.connect(host='localhost', dbname='dbdata', user='postgres', password='Q1w2e3r4')
 cursor = connection.cursor()
 try:
     sel_query = """SELECT * FROM public.food"""
@@ -16,32 +15,30 @@ except psycopg2.errors.UndefinedTable:
     cursor.execute(sel_query)
 
 array = list(cursor.fetchall())
-
 bot = telebot.TeleBot('841097550:AAFc5MoFRivTEfv-gOSJctqH53NfYMTKpCc')
-
 
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("Хот-доги и соусы!")
-    item2 = types.KeyboardButton("Случайная фотография нашего товара!")
+    item1 = types.KeyboardButton("Хот-доги и не только!")
+    item2 = types.KeyboardButton("Цена товара")
     markup.add(item1)
     markup.add(item2)
     bot.send_message(m.chat.id,
-                     '"Хот-доги и соусы!" - для получения случайного товара из нашей хотдожной\n'
-                     ' "Случайная фотография нашего товара!" - для вывода фотографий',
-                     reply_markup=markup)
+                     '"Хот-доги и не только!" - для получения случайного товара из нашей хотдожной\n'
+                     ' "Цена товара" - для вывода цены', reply_markup=markup)
 
+i = random.choice(array)
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
-    if (message.text.strip() == 'Хот-доги и соусы!'):
+    global i
+    if (message.text.strip() == 'Хот-доги и не только!'):
         i = random.choice(array)
-        ans = f'Название: {i[1]}\nЦена: {i[2]}\n'
+        ans = f'Название: {i[1]}\n'
         bot.send_message(message.chat.id, ans)
         bot.send_photo(message.chat.id, open(i[3], 'rb'))
-    elif (message.text.strip() == 'Случайная фотография нашего товара!'):
-        i = random.choice(array)
-        bot.send_photo(message.chat.id, open(i[3], 'rb'))
-
+    elif (message.text.strip() == 'Цена товара'):
+        ans = f'Цена: {i[2]}\n'
+        bot.send_message(message.chat.id, ans)
 
 bot.polling(none_stop=True, interval=0)
