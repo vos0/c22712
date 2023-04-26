842f08f77337084081675e4d79c065e1dbe126ae
Harutyun <arutgaranyan2005@gmail.com>
2023-04-26 14:09:41 +0300

    attempt to create telegramBot

--- Задания/task1/Garanyan/tgbot.py
+++ Задания/task1/Garanyan/tgbot.py
@@ -2,7 +2,7 @@ import telebot
 import psycopg2
 from telebot import types
 import random
-#hello
+
 connection = psycopg2.connect(host='localhost', dbname='dbdata', user='postgres',
                               password='Q1w2e3r4')
 cursor = connection.cursor()
@@ -11,8 +11,7 @@ try:
     cursor.execute(sel_query)
 except psycopg2.errors.UndefinedTable:
     connection.rollback()
-    import tableCreator
-
+    import problems
     sel_query = """SELECT * FROM public.food"""
     cursor.execute(sel_query)
 
@@ -25,13 +24,13 @@ bot = telebot.TeleBot('841097550:AAFc5MoFRivTEfv-gOSJctqH53NfYMTKpCc')
 def start(m, res=False):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton("Хот-доги и соусы!")
-    item2 = types.KeyboardButton("Повтори!")
+    item2 = types.KeyboardButton("Случайная фотография нашего товара!")
     markup.add(item1)
     markup.add(item2)
     bot.send_message(m.chat.id,
-                     '"Хот-доги и соусы!" - для получения случайного товара из нашей хотдожной\n "Повтори!" - для повторения сообщений.',
+                     '"Хот-доги и соусы!" - для получения случайного товара из нашей хотдожной\n'
+                     ' "Случайная фотография нашего товара!" - для вывода фотографий',
                      reply_markup=markup)
-
 
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
@@ -40,8 +39,9 @@ def handle_text(message):
         ans = f'Название: {i[1]}\nЦена: {i[2]}\n'
         bot.send_message(message.chat.id, ans)
         bot.send_photo(message.chat.id, open(i[3], 'rb'))
-    elif (message.text.strip() == 'Повтори!'):
-        bot.send_message(message.chat.id, 'Ввод: ' + message.text)
+    elif (message.text.strip() == 'Случайная фотография нашего товара!'):
+        i = random.choice(array)
+        bot.send_photo(message.chat.id, open(i[3], 'rb'))
 
 
 bot.polling(none_stop=True, interval=0)
